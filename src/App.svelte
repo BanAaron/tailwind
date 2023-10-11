19dfd6fe4b41dd3d7d07f3bee99f500ad3abc618
aarontbarratt <aarontbarratt@gmail.com>
2023-10-11 01:54:28 +0100

    1 first commit

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<main>
+    <div id="header" class="flex-1 justify-center text-center">
+        <h1 class="text-5xl p-8 pb-4 text-white font-mono hover:underline">aaronbarratt.dev</h1>
+        <p class="text-indigo-50 font-mono text-2xl">-----------------------</p>
+    </div>
+</main>
