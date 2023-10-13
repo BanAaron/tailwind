10df7f04e230a24d1003d0808d14adf2d5dff3e9
aarontbarratt <aarontbarratt@gmail.com>
2023-10-13 01:19:31 +0100

    1 first commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,33 @@
-<main>
-    <div id="header" class="flex-1 justify-center text-center">
-        <h1 class="text-5xl p-8 pb-4 text-white font-mono hover:underline">aaronbarratt.dev</h1>
-        <p class="text-indigo-50 font-mono text-2xl">-----------------------</p>
+<div id="container" class="w-screen h-auto bg-gradient-to-tr from-slate-900 to-slate-800 via-indigo-950-800">
+    <div class="p-7">
+        <div id="header" class="">
+            <ul class="flex justify-evenly font-mono text-indigo-100 space-x-3">
+                <li><span class="text-green-300">01.</span> about</li>
+                <li><span class="text-green-300">02.</span> work</li>
+                <li><span class="text-green-300">03.</span> projects</li>
+            </ul>
+        </div>
+        <main>
+            <div class="mt-20 flex h-60 flex-col text-indigo-100 space-y-3">
+                <p class="font-mono text-xl text-green-300">Hi, my name is</p>
+                <h1 class="text-5xl font-bold">Aaron Barratt</h1>
+                <h2 class="text-2xl text-slate-400 font-semibold"><span class="text-green-300 font-semibold">> </span>Full-stack
+                    Developer</h2>
+            </div>
+            <div id="jobs" class="flex flex-wrap text-indigo-100 space-y-4">
+                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
+                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
+                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
+                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
+                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
+                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
+                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
+                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
+                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
+                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
+                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
+                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
+            </div>
+        </main>
     </div>
-</main>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}', 'index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var'],
+      },
+    },
   },
   plugins: [],
 }
