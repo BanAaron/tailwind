007c37f29e58970040d75ad19fe92e7c1313050a
aarontbarratt <aarontbarratt@gmail.com>
2023-10-14 23:53:31 +0100

    good progress

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,60 @@
-<div id="container" class="w-screen h-auto bg-gradient-to-tr from-slate-900 to-slate-800 via-indigo-950-800">
-    <div class="p-7">
-        <div id="header" class="">
-            <ul class="flex justify-evenly font-mono text-indigo-100 space-x-3">
-                <li><span class="text-green-300">01.</span> about</li>
-                <li><span class="text-green-300">02.</span> work</li>
-                <li><span class="text-green-300">03.</span> projects</li>
-            </ul>
-        </div>
-        <main>
-            <div class="mt-20 flex h-60 flex-col text-indigo-100 space-y-3">
-                <p class="font-mono text-xl text-green-300">Hi, my name is</p>
-                <h1 class="text-5xl font-bold">Aaron Barratt</h1>
-                <h2 class="text-2xl text-slate-400 font-semibold"><span class="text-green-300 font-semibold">> </span>Full-stack
-                    Developer</h2>
-            </div>
-            <div id="jobs" class="flex flex-wrap text-indigo-100 space-y-4">
-                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
-                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
-                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
-                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
-                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
-                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
-                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
-                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
-                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
-                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
-                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
-                <div id="child" class="flex h-24 w-full items-center justify-center shadow-black bg-blue-950 xl:w-1/4"><p>job</p></div>
-            </div>
-        </main>
+<div id="container" class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+  <div id="splash" class="h-screen w-auto col-start-1 col-end-1 content-center justify-center
+  xl:col-start-1 xl:col-end-2
+">
+    <div
+      class="flex h-screen flex-col items-center justify-center bg-gradient-to-tr from-slate-900 to-slate-800 space-y-2.5 via-indigo-950-800">
+      <img class="m-3 h-64 w-auto rounded-full object-left shadow-xl dark:shadow-gray-800" src="src/assets/aaron.jpeg"
+           alt="selfie of Aaron Barratt">
+      <h1 class="text-4xl font-bold text-slate-200 md:text-5xl">Aaron Barratt</h1>
+      <h2 class="text-2xl text-slate-300 md:text-3xl">Developer - <a href="https://www.dentsu.com/">Dentsu</a></h2>
+      <div class="flex text-slate-400 gap-x-2.5">
+        <a href="https://github.com/banaaron" target="_blank">
+          <svg class="h-10 w-auto text-slate-300 hover:animate-spin hover:text-slate-50" viewBox="0 0 24 24" fill="none"
+               stroke="currentColor" stroke-width="1.5"
+               stroke-linecap="round" stroke-linejoin="round">
+            <path
+              d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"/>
+          </svg>
+        </a>
+        <a href="https://www.linkedin.com/in/aaron-barratt/" target="_blank">
+          <svg class="h-10 w-auto text-slate-300 hover:animate-spin hover:text-slate-50" viewBox="0 0 24 24" fill="none"
+               stroke="currentColor" stroke-width="1.5"
+               stroke-linecap="round" stroke-linejoin="round">
+            <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/>
+            <rect x="2" y="9" width="4" height="12"/>
+            <circle cx="4" cy="4" r="2"/>
+          </svg>
+        </a>
+        <a href="https://twitter.com/aarontbarratt" target="_blank">
+          <svg class="h-10 w-auto text-slate-300 hover:animate-spin hover:text-slate-50"
+               viewBox="0 0 24 24"
+               fill="none"
+               stroke="currentColor"
+               stroke-width="1.5"
+               stroke-linecap="round"
+               stroke-linejoin="round">
+            <path
+              d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"/>
+          </svg>
+        </a>
+        <a href="https://www.instagram.com/aarontbarratt" target="_blank">
+          <svg class="h-10 w-auto text-slate-300 hover:animate-spin hover:text-slate-50"
+               viewBox="0 0 24 24"
+               fill="none"
+               stroke="currentColor"
+               stroke-width="1.5"
+               stroke-linecap="round"
+               stroke-linejoin="round">
+            <rect x="2" y="2" width="20" height="20" rx="5" ry="5"/>
+            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/>
+            <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"/>
+          </svg>
+        </a>
+      </div>
     </div>
+  </div>
+  <div class="flex items-center justify-center h-screen bg-pink-600 col-start-1 col-end-2 md:col-start-2 md:col-end-3 lg:col-start-2 lg:col-end-4">
+    <h1>some content</h1>
+  </div>
 </div>
