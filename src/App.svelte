091d706945ccfd778072ccb854711204ed95ea1b
Aaron Barratt <aarontbarratt@gmail.com>
2023-10-15 18:44:44 +0100

    finished work section
    starting projects section

--- src/App.svelte
+++ src/App.svelte
@@ -17,17 +17,25 @@
       <h2 class="text-2xl text-slate-300 md:text-2xl">Full-stack Developer</h2>
       <div class="flex gap-x-2.5">
         <a href="https://github.com/banaaron" target="_blank">
-          <svg class="h-8 w-auto text-slate-400 hover:text-teal-300" viewBox="0 0 24 24" fill="none"
-               stroke="currentColor" stroke-width="1.5"
-               stroke-linecap="round" stroke-linejoin="round">
+          <svg class="h-8 w-auto text-slate-400 hover:text-teal-300"
+               viewBox="0 0 24 24"
+               fill="none"
+               stroke="currentColor"
+               stroke-width="1.5"
+               stroke-linecap="round"
+               stroke-linejoin="round">
             <path
               d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"/>
           </svg>
         </a>
         <a href="https://www.linkedin.com/in/aaron-barratt/" target="_blank">
-          <svg class="h-8 w-auto text-slate-400 hover:text-teal-300" viewBox="0 0 24 24" fill="none"
-               stroke="currentColor" stroke-width="1.5"
-               stroke-linecap="round" stroke-linejoin="round">
+          <svg class="h-8 w-auto text-slate-400 hover:text-teal-300"
+               viewBox="0 0 24 24"
+               fill="none"
+               stroke="currentColor"
+               stroke-width="1.5"
+               stroke-linecap="round"
+               stroke-linejoin="round">
             <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/>
             <rect x="2" y="9" width="4" height="12"/>
             <circle cx="4" cy="4" r="2"/>
@@ -62,25 +70,120 @@
     </div>
   </div>
   <div
-    class="col-start-1 col-end-2 flex h-screen flex-col justify-center bg-gradient-to-b from-slate-900 p-7 text-base text-slate-400 space-y-3 to-slate-950 md:from-20% md:from-slate-950 md:col-start-2 md:col-end-3 md:bg-gradient-to-r md:to-slate-900 lg:col-start-2 lg:col-end-4">
-    <h1 id="about" class="text-xl font-extrabold tracking-wide text-slate-100 "><a href="#about"
-                                                                                   class="hover:text-teal-300">ABOUT</a>
-    </h1>
-    <p>For me, it all started in 2006, playing Age of Empires 2 and EverQuest with my Dad on his "work computer". From
-      then on I
-      was obsessed with video games and how they worked.</p>
-    <p>After many years (and too many games of StarCraft 2) I decided to study <a
-      class="text-slate-300 hover:text-teal-300"
-      href="https://www.northampton.ac.uk/courses/games-programming-bsc-hons/">Computer
-      Games Programming</a> at the University of Northampton.</p>
-    <p>It turns out that making video games isn't quite as fun as playing them! Instead I discovered my passion was for
-      data and web development.</p>
-    <p>Once it was time to enter the world of work I decided to learn Python, JavaScript, and become a SQL expert.</p>
+    class="col-start-1 col-end-2 flex h-screen flex-col justify-center bg-gradient-to-b from-slate-900 p-7 text-sm text-slate-400 space-y-3 to-slate-950 md:from-20% md:from-slate-950 md:col-start-2 md:col-end-3 md:bg-gradient-to-r md:to-slate-900 lg:col-start-2 lg:col-end-4">
+    <h1 id="about" class="text-xl font-extrabold uppercase tracking-wide text-slate-100"><a href="#about" class="hover:text-teal-300">About</a></h1>
+    <p>
+      Back in 2006 I started playing EverQuest with my Dad on his "work computer". Since then on I have been obsessed with video games, tech, and programming.
+    </p>
+    <p>
+      After many years (and too many games of StarCraft 2) I decided to study <a class="text-slate-300 hover:text-teal-300" href="https://www.northampton.ac.uk/courses/games-programming-bsc-hons/"
+                                                                                 target="_blank">Computer
+      Games Programming</a> at the University of Northampton.
+    </p>
+    <p>
+      It turns out that making video games isn't quite as fun as playing them! Instead I discovered my passion was in <a href="#welcome-break" class="text-slate-300 hover:text-teal-300">databases</a>
+      and <a href="#dentsu" class="text-slate-300 hover:text-teal-300">web development</a>.
+    </p>
+    <p>
+      Fast forward today and I have had the privilege to work in such roles as <a class="text-slate-300 hover:text-teal-300" href="#welcome-break">database administrator</a>, <a href="#userzoom"
+                                                                                                                                                                                  class="text-slate-300 hover:text-teal-300">support
+      engineer</a>, and currently a <a href="#dentsu" class="text-slate-300 hover:text-teal-300">full-stack developer</a> at <a href="https://www.dentsu.com/uk/en"
+                                                                                                                                target="_blank"
+                                                                                                                                class="text-slate-300 hover:text-teal-300">Dentsu</a>
+    </p>
+  </div>
+  <div
+    class="col-start-1 col-end-2 flex h-auto flex-col justify-center from-slate-900 p-7 text-base text-slate-400 bg-slate-950 space-y-3 md:from-20% md:from-slate-950 md:col-start-2 md:col-end-3 md:bg-gradient-to-r md:to-slate-900 lg:col-start-2 lg:col-end-4">
+    <h1 id="work" class="text-xl font-extrabold uppercase tracking-wide text-slate-100"><a href="#work" class="hover:text-teal-300">Work</a></h1>
+    <div id="jobs" class="grid grid-cols-1 gap-7">
+      <div id="job" class="text-sm space-y-1">
+        <header id="dentsu" class="text-xs font-semibold uppercase tracking-wide text-slate-400">2023 — present</header>
+        <h1 id="job-title" class="text-xl font-semibold text-slate-200"><a href="https://www.dentsu.com" target="_blank" class="hover:text-teal-300">Full-stack Developer · Dentsu</a></h1>
+        <h2 id="city" class="text-slate-200">London</h2>
+        <p>
+          I design and develop robust web applications, ensuring a seamless user experience from front-end to back-end.
+        </p>
+        <p>
+          My role is to transform innovative concepts into digital reality, delivering excellence in the world of web development.
+        </p>
+      </div>
+      <div id="job" class="text-sm space-y-1">
+        <header id="userzoom" class="text-xs font-semibold uppercase tracking-wide text-slate-400">2020 — 2023</header>
+        <h1 id="job-title" class="text-xl font-semibold text-slate-200"><a href="https://userzoom.com/" target="_blank" class="hover:text-teal-300">T3 Support · UserZoom</a></h1>
+        <h2 id="city" class="text-slate-200">Manchester</h2>
+        <p>
+          I refactored the KPI ETL process saving over 16 hours or run time per week.
+        </p>
+        <p>
+          In-depth expertise in troubleshooting and problem-solving, I assist in resolving complex issues,
+          ensuring minimal downtime and maximum system efficiency
+        </p>
+      </div>
+      <div id="job" class="text-sm space-y-1">
+        <header id="welcome-break" class="text-xs font-semibold uppercase tracking-wide text-slate-400">2019 — 2020</header>
+        <h1 id="job-title" class="text-xl font-semibold text-slate-200"><a href="https://www.welcomebreak.co.uk/" target="_blank" class="hover:text-teal-300">Database Admin · W. Break</a></h1>
+        <h2 id="city" class="text-slate-200">Milton Keynes</h2>
+        <p>
+          I specialized in managing and optimizing data systems to ensure the integrity, security, and performance of critical information.
+        </p>
+        <p>
+          My role involves fine-tuning SQL queries, implementing disaster recovery plans, and ensuring data availability.
+        </p>
+      </div>
+      <div id="job" class="text-sm space-y-1">
+        <header id="invu" class="text-xs font-semibold uppercase tracking-wide text-slate-400">2018 — 2019</header>
+        <h1 id="job-title" class="text-xl font-semibold text-slate-200"><a href="https://www.agilico.co.uk/invu/" target="_blank" class="hover:text-teal-300">Professional Services · Invu</a></h1>
+        <h2 id="city" class="text-slate-200">Northampton</h2>
+        <p>
+          Developed custom optical recognition processing solutions using C#.
+        </p>
+        <p>
+          Implemented a £100,000+ invoice processing automation solution.
+        </p>
+      </div>
+      <div id="job" class="text-sm space-y-1">
+        <header id="invu" class="text-xs font-semibold uppercase tracking-wide text-slate-400">2017 — 2018</header>
+        <h1 id="job-title" class="text-xl font-semibold text-slate-200"><a href="https://www.agilico.co.uk/invu/" target="_blank" class="hover:text-teal-300">Technical Support · Invu</a></h1>
+        <h2 id="city" class="text-slate-200">Northampton</h2>
+        <p>
+          Provided excellent third line technical support to over 1,800 customers across the UK.
+        </p>
+        <p>
+          Researched, piloted and implemented ZenDesk into the support team. Thoroughly document technical issues and solutions.
+        </p>
+      </div>
+    </div>
   </div>
   <div
     class="col-start-1 col-end-2 flex h-screen flex-col justify-center from-slate-900 p-7 text-base text-slate-400 bg-slate-950 space-y-3 md:from-20% md:from-slate-950 md:col-start-2 md:col-end-3 md:bg-gradient-to-r md:to-slate-900 lg:col-start-2 lg:col-end-4">
-    <h1 id="work" class="text-xl font-extrabold tracking-wide text-slate-100"><a href="#work"
-                                                                                 class="hover:text-teal-300">WORK</a>
-    </h1>
+    <h1 id="projects" class="text-xl font-extrabold uppercase tracking-wide text-slate-100"><a href="#projects" class="hover:text-teal-300">Projects</a></h1>
+    <div id="projects" class="grid grid-cols-1 gap-7">
+      <div id="project" class="text-sm space-y-1">
+        <header id="job-title" class="text-xl font-semibold text-slate-200">
+          <a href="https://github.com/BanAaron/aaron-barratt-website" target="_blank" class="hover:text-teal-300">aaronbarratt.dev</a>
+        </header>
+        <p>
+          This website! Made to show off my HTML, CSS, and design skills. Built with <a href="https://tailwindcss.com/" target="_blank" class="text-slate-300 hover:text-teal-300">Svelte</a> and <a
+          href="https://tailwindcss.com/" target="_blank" class="text-slate-300 hover:text-teal-300">Tailwind</a>.
+        </p>
+        <p>
+          Using  eactive design principles to work flawlessly for mobile and desktop.
+        </p>
+        <ul class="flex flex-wrap gap-2">
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Svelte</div>
+          </li>
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Tailwind</div>
+          </li>
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">HTML</div>
+          </li>
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Design</div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </div>
