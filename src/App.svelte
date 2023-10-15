a455468bd542fd7f5b8bbea9d3a339e96843a3e4
Aaron Barratt <aarontbarratt@gmail.com>
2023-10-15 13:56:17 +0100

    fixed mobile and desktop layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,23 @@
-<div id="container" class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-  <div id="splash" class="h-screen w-auto col-start-1 col-end-1 content-center justify-center
-  xl:col-start-1 xl:col-end-2
-">
+<div id="container" class="grid grid-cols-1 text-slate-50 md:grid-cols-2 xl:grid-cols-3">
+  <div id="burger" class="fixed top-0 right-0 z-50 m-5 origin-right md:hidden">
+    <div class="space-y-2">
+      <div class="w-8 bg-gray-400 h-0.5"></div>
+      <div class="w-8 bg-gray-400 h-0.5"></div>
+      <div class="w-8 bg-gray-400 h-0.5"></div>
+    </div>
+  </div>
+  <div id="splash"
+       class="col-start-1 col-end-1 h-screen w-auto content-center justify-center md:fixed md:w-1/2 xl:fixed xl:w-1/3">
     <div
-      class="flex h-screen flex-col items-center justify-center bg-gradient-to-tr from-slate-900 to-slate-800 space-y-2.5 via-indigo-950-800">
-      <img class="m-3 h-64 w-auto rounded-full object-left shadow-xl dark:shadow-gray-800" src="src/assets/aaron.jpeg"
+      class="flex h-screen flex-col items-center justify-center bg-gradient-to-b from-slate-800 to-slate-900 space-y-2.5 md:to-45% md:to-slate-950 md:bg-gradient-to-r md:from-slate-900">
+      <img class="m-3 h-64 w-auto rounded-full border-2 border-teal-300 object-left shadow-xl dark:shadow-gray-800"
+           src="src/assets/aaron.jpeg"
            alt="selfie of Aaron Barratt">
-      <h1 class="text-4xl font-bold text-slate-200 md:text-5xl">Aaron Barratt</h1>
-      <h2 class="text-2xl text-slate-300 md:text-3xl">Developer - <a href="https://www.dentsu.com/">Dentsu</a></h2>
-      <div class="flex text-slate-400 gap-x-2.5">
+      <a href="/"><h1 class="text-5xl font-semibold text-white md:text-5xl">Aaron Barratt</h1></a>
+      <h2 class="text-2xl text-slate-300 md:text-2xl">Full-stack Developer</h2>
+      <div class="flex gap-x-2.5">
         <a href="https://github.com/banaaron" target="_blank">
-          <svg class="h-10 w-auto text-slate-300 hover:animate-spin hover:text-slate-50" viewBox="0 0 24 24" fill="none"
+          <svg class="h-8 w-auto text-slate-400 hover:text-teal-300" viewBox="0 0 24 24" fill="none"
                stroke="currentColor" stroke-width="1.5"
                stroke-linecap="round" stroke-linejoin="round">
             <path
@@ -18,7 +25,7 @@
           </svg>
         </a>
         <a href="https://www.linkedin.com/in/aaron-barratt/" target="_blank">
-          <svg class="h-10 w-auto text-slate-300 hover:animate-spin hover:text-slate-50" viewBox="0 0 24 24" fill="none"
+          <svg class="h-8 w-auto text-slate-400 hover:text-teal-300" viewBox="0 0 24 24" fill="none"
                stroke="currentColor" stroke-width="1.5"
                stroke-linecap="round" stroke-linejoin="round">
             <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/>
@@ -27,7 +34,7 @@
           </svg>
         </a>
         <a href="https://twitter.com/aarontbarratt" target="_blank">
-          <svg class="h-10 w-auto text-slate-300 hover:animate-spin hover:text-slate-50"
+          <svg class="h-8 w-auto text-slate-400 hover:text-teal-300"
                viewBox="0 0 24 24"
                fill="none"
                stroke="currentColor"
@@ -39,7 +46,7 @@
           </svg>
         </a>
         <a href="https://www.instagram.com/aarontbarratt" target="_blank">
-          <svg class="h-10 w-auto text-slate-300 hover:animate-spin hover:text-slate-50"
+          <svg class="h-8 w-auto text-slate-400 hover:text-teal-300"
                viewBox="0 0 24 24"
                fill="none"
                stroke="currentColor"
@@ -54,7 +61,22 @@
       </div>
     </div>
   </div>
-  <div class="flex items-center justify-center h-screen bg-pink-600 col-start-1 col-end-2 md:col-start-2 md:col-end-3 lg:col-start-2 lg:col-end-4">
-    <h1>some content</h1>
+  <div
+    class="col-start-1 col-end-2 flex h-screen flex-col justify-center bg-gradient-to-b from-slate-900 p-7 text-base text-slate-400 space-y-3 to-slate-950 md:from-20% md:from-slate-950 md:col-start-2 md:col-end-3 md:bg-gradient-to-r md:to-slate-900 lg:col-start-2 lg:col-end-4">
+    <h1 id="about" class="text-xl font-extrabold tracking-wide text-slate-100">ABOUT</h1>
+    <p>For me, it all started in 2006, playing Age of Empires 2 and EverQuest with my Dad on his "work computer". From
+      then on I
+      was obsessed with video games and how they worked.</p>
+    <p>After many years (and too many games of StarCraft 2) I decided to study <a
+      class="text-slate-300 hover:text-teal-300"
+      href="https://www.northampton.ac.uk/courses/games-programming-bsc-hons/">Computer
+      Games Programming</a> at the University of Northampton.</p>
+    <p>It turns out that making video games isn't quite as fun as playing them! Instead I discovered my passion was for
+      data and web development.</p>
+    <p>Once it was time to enter the world of work I decided to learn Python, JavaScript, and become a SQL expert.</p>
+  </div>
+  <div
+    class="col-start-1 col-end-2 flex h-screen flex-col justify-center from-slate-900 p-7 text-base text-slate-400 bg-slate-950 space-y-3 md:from-20% md:from-slate-950 md:col-start-2 md:col-end-3 md:bg-gradient-to-r md:to-slate-900 lg:col-start-2 lg:col-end-4">
+    <a id="work"><h1 class="text-xl font-extrabold tracking-wide text-slate-100">WORK</h1></a>
   </div>
 </div>
