7ac86158d1ed3ce46a25dab863d9b5ce5573a8df
aarontbarratt <aarontbarratt@gmail.com>
2023-10-16 11:30:53 +0100

    finished projects
    added footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <div id="container" class="grid grid-cols-1 text-slate-50 md:grid-cols-2 xl:grid-cols-3">
+  <!-- mobile burger menu -->
   <div id="burger" class="fixed top-0 right-0 z-50 m-5 origin-right md:hidden">
     <div class="space-y-2">
       <div class="w-8 bg-gray-400 h-0.5"></div>
@@ -6,6 +7,7 @@
       <div class="w-8 bg-gray-400 h-0.5"></div>
     </div>
   </div>
+  <!-- splash screen section -->
   <div id="splash"
        class="col-start-1 col-end-1 h-screen w-auto content-center justify-center md:fixed md:w-1/2 xl:fixed xl:w-1/3">
     <div
@@ -69,6 +71,7 @@
       </div>
     </div>
   </div>
+  <!-- about me section -->
   <div
     class="col-start-1 col-end-2 flex h-screen flex-col justify-center bg-gradient-to-b from-slate-900 p-7 text-sm text-slate-400 space-y-3 to-slate-950 md:from-20% md:from-slate-950 md:col-start-2 md:col-end-3 md:bg-gradient-to-r md:to-slate-900 lg:col-start-2 lg:col-end-4">
     <h1 id="about" class="text-xl font-extrabold uppercase tracking-wide text-slate-100"><a href="#about" class="hover:text-teal-300">About</a></h1>
@@ -92,10 +95,12 @@
                                                                                                                                 class="text-slate-300 hover:text-teal-300">Dentsu</a>
     </p>
   </div>
+  <!-- jobs section -->
   <div
     class="col-start-1 col-end-2 flex h-auto flex-col justify-center from-slate-900 p-7 text-base text-slate-400 bg-slate-950 space-y-3 md:from-20% md:from-slate-950 md:col-start-2 md:col-end-3 md:bg-gradient-to-r md:to-slate-900 lg:col-start-2 lg:col-end-4">
     <h1 id="work" class="text-xl font-extrabold uppercase tracking-wide text-slate-100"><a href="#work" class="hover:text-teal-300">Work</a></h1>
     <div id="jobs" class="grid grid-cols-1 gap-7">
+      <!-- Dentsu -->
       <div id="job" class="text-sm space-y-1">
         <header id="dentsu" class="text-xs font-semibold uppercase tracking-wide text-slate-400">2023 — present</header>
         <h1 id="job-title" class="text-xl font-semibold text-slate-200"><a href="https://www.dentsu.com" target="_blank" class="hover:text-teal-300">Full-stack Developer · Dentsu</a></h1>
@@ -107,6 +112,7 @@
           My role is to transform innovative concepts into digital reality, delivering excellence in the world of web development.
         </p>
       </div>
+      <!-- UserZoom -->
       <div id="job" class="text-sm space-y-1">
         <header id="userzoom" class="text-xs font-semibold uppercase tracking-wide text-slate-400">2020 — 2023</header>
         <h1 id="job-title" class="text-xl font-semibold text-slate-200"><a href="https://userzoom.com/" target="_blank" class="hover:text-teal-300">T3 Support · UserZoom</a></h1>
@@ -119,6 +125,7 @@
           ensuring minimal downtime and maximum system efficiency
         </p>
       </div>
+      <!-- welcome break -->
       <div id="job" class="text-sm space-y-1">
         <header id="welcome-break" class="text-xs font-semibold uppercase tracking-wide text-slate-400">2019 — 2020</header>
         <h1 id="job-title" class="text-xl font-semibold text-slate-200"><a href="https://www.welcomebreak.co.uk/" target="_blank" class="hover:text-teal-300">Database Admin · W. Break</a></h1>
@@ -130,6 +137,7 @@
           My role involves fine-tuning SQL queries, implementing disaster recovery plans, and ensuring data availability.
         </p>
       </div>
+      <!-- invu -->
       <div id="job" class="text-sm space-y-1">
         <header id="invu" class="text-xs font-semibold uppercase tracking-wide text-slate-400">2018 — 2019</header>
         <h1 id="job-title" class="text-xl font-semibold text-slate-200"><a href="https://www.agilico.co.uk/invu/" target="_blank" class="hover:text-teal-300">Professional Services · Invu</a></h1>
@@ -141,6 +149,7 @@
           Implemented a £100,000+ invoice processing automation solution.
         </p>
       </div>
+      <!-- invu -->
       <div id="job" class="text-sm space-y-1">
         <header id="invu" class="text-xs font-semibold uppercase tracking-wide text-slate-400">2017 — 2018</header>
         <h1 id="job-title" class="text-xl font-semibold text-slate-200"><a href="https://www.agilico.co.uk/invu/" target="_blank" class="hover:text-teal-300">Technical Support · Invu</a></h1>
@@ -154,12 +163,14 @@
       </div>
     </div>
   </div>
+  <!-- projects -->
   <div
-    class="col-start-1 col-end-2 flex h-screen flex-col justify-center from-slate-900 p-7 text-base text-slate-400 bg-slate-950 space-y-3 md:from-20% md:from-slate-950 md:col-start-2 md:col-end-3 md:bg-gradient-to-r md:to-slate-900 lg:col-start-2 lg:col-end-4">
+    class="col-start-1 col-end-2 flex h-auto flex-col justify-center from-slate-900 p-7 text-base text-slate-400 bg-slate-950 space-y-3 md:from-20% md:from-slate-950 md:col-start-2 md:col-end-3 md:bg-gradient-to-r md:to-slate-900 lg:col-start-2 lg:col-end-4">
     <h1 id="projects" class="text-xl font-extrabold uppercase tracking-wide text-slate-100"><a href="#projects" class="hover:text-teal-300">Projects</a></h1>
     <div id="projects" class="grid grid-cols-1 gap-7">
+      <!-- aaronbarratt.dev -->
       <div id="project" class="text-sm space-y-1">
-        <header id="job-title" class="text-xl font-semibold text-slate-200">
+        <header id="project-title" class="text-xl font-semibold text-slate-200">
           <a href="https://github.com/BanAaron/aaron-barratt-website" target="_blank" class="hover:text-teal-300">aaronbarratt.dev</a>
         </header>
         <p>
@@ -167,11 +178,14 @@
           href="https://tailwindcss.com/" target="_blank" class="text-slate-300 hover:text-teal-300">Tailwind</a>.
         </p>
         <p>
-          Using eactive design principles to work flawlessly for mobile and desktop.
+          Using active design principles to work flawlessly for mobile and desktop.
         </p>
         <ul class="flex flex-wrap gap-2">
           <li class="mt-1">
             <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Svelte</div>
+          </li>
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">JavaScript</div>
           </li>
           <li class="mt-1">
             <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Tailwind</div>
@@ -184,6 +198,102 @@
           </li>
         </ul>
       </div>
+      <!-- resolution changer -->
+      <div id="project" class="text-sm space-y-1">
+        <header id="project-title" class="text-xl font-semibold text-slate-200">
+          <a href="https://github.com/BanAaron/resolution-changer" target="_blank" class="hover:text-teal-300">Resolution Changer</a>
+        </header>
+        <p>
+          A simple system tray application to switch between resolutions with 1 or 2 clicks.
+        </p>
+        <p>
+          I made this to quickly switch between 16:9 for gaming and 32:9 for productivity.
+        </p>
+        <ul class="flex flex-wrap gap-2">
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Python</div>
+          </li>
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Pystray</div>
+          </li>
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Windows API</div>
+          </li>
+        </ul>
+      </div>
+      <!-- image cropper -->
+      <div id="project" class="text-sm space-y-1">
+        <header id="project-title" class="text-xl font-semibold text-slate-200">
+          <a href="https://github.com/BanAaron/image-cropper" target="_blank" class="hover:text-teal-300">Image Cropper</a>
+        </header>
+        <p>
+          A simple GUI application to bulk crop images. Made using the <a href="https://pypi.org/project/Pillow/" class="text-slate-300 hover:text-teal-300">Pillow</a> image processing library.
+        </p>
+        <ul class="flex flex-wrap gap-2">
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Python</div>
+          </li>
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Pillow</div>
+          </li>
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Tkinter</div>
+          </li>
+        </ul>
+      </div>
+      <!-- tekken -->
+      <div id="project" class="text-sm space-y-1">
+        <header id="project-title" class="text-xl font-semibold text-slate-200">
+          <a href="https://github.com/BanAaron/tekken-backend" target="_blank" class="hover:text-teal-300">Tekken 8 API</a>
+        </header>
+        <p>
+          A simple API written in Go. You can request character data for <a href="https://youtu.be/2hPuRQz6IlM" target="_blank" class="text-slate-300 hover:text-teal-300">Tekken 8</a>. This is a work
+          in progress.
+        </p>
+        <p>
+          I plan to build my own site that consumes this data to help players learn the game.
+        </p>
+        <ul class="flex flex-wrap gap-2">
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Go</div>
+          </li>
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">API</div>
+          </li>
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">PostgreSQL</div>
+          </li>
+          <li class="mt-1">
+            <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Supabase</div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!-- footer -->
+    <div
+      class="col-start-1 col-end-2 flex h-auto flex-col justify-center from-slate-900 pt-14 text-base text-slate-400 bg-slate-950 space-y-3 md:from-20% md:from-slate-950 md:col-start-2 md:col-end-3 md:bg-gradient-to-r md:to-slate-900 lg:col-start-2 lg:col-end-4">
+      <footer>
+        <p class="text-xs">
+          Coded with <a href="https://www.jetbrains.com/webstorm/" class="text-slate-300 hover:text-teal-300">IntelliJ</a>.
+          Built with <a href="https://svelte.dev/" class="text-slate-300 hover:text-teal-300">Svelte</a>
+          and <a href="https://svelte.dev/" class="text-slate-300 hover:text-teal-300">Tailwind</a>.
+          Deployed by <a href="https://svelte.dev/" class="text-slate-300 hover:text-teal-300">Vercel</a>
+          ❤️
+        </p>
+        <div class="flex justify-center">
+          <a href="#splash">
+            <svg class="mt-2 h-10 fill-slate-300 hover:fill-teal-300" viewBox="-3 0 32 32" xmlns="http://www.w3.org/2000/svg">
+              <g id="icomoon-ignore">
+              </g>
+              <path
+                d="M26.221 16c0-7.243-5.871-13.113-13.113-13.113s-13.113 5.87-13.113 13.113c0 7.242 5.871 13.113 13.113 13.113s13.113-5.871 13.113-13.113zM1.045 16c0-6.652 5.412-12.064 12.064-12.064s12.064 5.412 12.064 12.064c0 6.652-5.411 12.064-12.064 12.064-6.652 0-12.064-5.412-12.064-12.064z">
+              </path>
+              <path d="M18.746 15.204l0.742-0.742-6.379-6.379-6.378 6.379 0.742 0.742 5.112-5.112v12.727h1.049v-12.727z">
+              </path>
+            </svg>
+          </a>
+        </div>
+      </footer>
     </div>
   </div>
 </div>
