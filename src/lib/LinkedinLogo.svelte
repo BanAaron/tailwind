f95f86d6af4cbcc2598fd85c1acaa5839b2c76fd
Khephren <aarontbarratt@gmail.com>
2023-11-05 00:33:50 +0000

    formatting pass

--- src/lib/LinkedinLogo.svelte
+++ src/lib/LinkedinLogo.svelte
@@ -1,12 +1,14 @@
-<svg class="h-8 w-auto text-slate-400 hover:text-teal-300"
-     fill="none"
-     stroke="currentColor"
-     aria-label="checkout my LinkedIn"
-     stroke-linecap="round"
-     stroke-linejoin="round"
-     stroke-width="1.5"
-     viewBox="0 0 24 24">
-  <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/>
-  <rect height="12" width="4" x="2" y="9"/>
-  <circle cx="4" cy="4" r="2"/>
+<svg
+	class="h-8 w-auto text-slate-400 hover:text-teal-300"
+	fill="none"
+	stroke="currentColor"
+	aria-label="checkout my LinkedIn"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	stroke-width="1.5"
+	viewBox="0 0 24 24"
+>
+	<path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+	<rect height="12" width="4" x="2" y="9" />
+	<circle cx="4" cy="4" r="2" />
 </svg>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { parse } from 'marked';
+
+	let markdown: string = `# Hello, World
+    My name is Aaron.
+    - bullet point
+    `;
+</script>
+
+<main>
+	<div class="preview">{@html parse(markdown)}</div>
+</main>
