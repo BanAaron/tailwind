0af43805b96ab1dc878e3b584296897cbf2cde56
Khephren <aarontbarratt@gmail.com>
2023-11-27 00:22:52 +0000

    scuffed but working

--- src/lib/DarkMode.svelte
+++ src/lib/DarkMode.svelte
@@ -1,12 +1,52 @@
-<script lang="ts">
+<script>
+
+    import {browser} from "$app/environment";
+
+    if (browser) {
+        const mask = document.querySelector('.shadow');
+
+        document.addEventListener('pointermove', (pos) => {
+            let x = parseInt(pos.clientX / window.innerWidth * 100);
+            let y = parseInt(pos.clientY / window.innerHeight * 100);
+
+            mask.style.setProperty('--mouse-x', x + '%');
+            mask.style.setProperty('--mouse-y', y + '%');
+        });
+    }
 
 </script>
 
 <div
-        class="fixed l-0 z-50 origin-left bg-black w-screen h-screen"
+        class="shadow"
 >
 
     <button>
         Dark mode
     </button>
 </div>
+
+<style>
+    :root {
+        --mouse-x: 50%;
+        --mouse-y: 50%;
+    }
+
+    .shadow {
+        position: fixed;
+        z-index: 999;
+        width: 100vw;
+        height: 100vh;
+        background-color: hsla(0, 0%, 0%, 0.9);
+        mask: radial-gradient(
+                circle at var(--mouse-x) var(--mouse-y),
+                transparent 40px,
+                black 150px
+        );
+        -webkit-mask: radial-gradient(
+                circle at var(--mouse-x) var(--mouse-y),
+                transparent 40px,
+                black 150px
+        );
+    }
+
+</style>
